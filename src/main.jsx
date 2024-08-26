2cffa9c69d9449aa323c3af30e86b651635bebe5
LeeWest89 <6683@holbertonstudents.com>
2024-08-26 13:16:32 -0500

    Now displaying Hello Atlas, Seems very similar to how I would use javascript to create sections of my html at the end of T3

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,9 @@
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
 // Render the App component to the root element
+const div = document.getElementById("root");
+const root = ReactDOM.createRoot(div);
+root.render(<App />);
